00fd63971ab2995f728a69d0600950c97d2a547d
Pashtet482 <dydaevmakx41@gmail.com>
2026-04-24 01:42:38 +0300

    initial commit

---
+++ src/main/java/com/example/autoshop/AutoshopApplication.java
@@ -0,0 +1,11 @@
+package com.example.autoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoshopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoshopApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/autoshop/AutoshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
